cc47e32c1408e1ec42db78bf01820aca5f81236f
winwin2011 <yjh2011@live.com>
2023-04-03 20:36:49 +0800

    feat: basic requestHandler

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+const http = require("http")
+const host = process.env.HOST || "localhost"
+const port = process.env.PORT || 3000
+
+const requestHandler = (req, res) => {
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/plain")
+  res.end("Hello World")
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, host, (err) => {
+  if (err) {
+    return console.log("something bad happened", err)
+  }
+
+  console.log(`server is listening on ${host}:${port}`)
+})
